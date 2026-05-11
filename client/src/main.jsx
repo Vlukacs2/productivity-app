8eee1fe49e37d37902bd0ab4b1ad0c84cc88b030
Vlukacs2 <vlukacs30@gmail.com>
2026-05-11 22:12:55 +0300

    login and registration frontend

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import App from "./App.jsx";
+import { AuthProvider } from "./context/AuthContext.jsx";
+import { TaskProvider } from "./context/TaskContext.jsx";
+
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <AuthProvider>
+        <TaskProvider>
+          <App />
+        </TaskProvider>
+      </AuthProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
