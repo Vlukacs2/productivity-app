2fd70c6bba3e876ad0a02bf1b727626d3ae8f277
wizardon1000mgcaffeine <stumpf.gyorgy0519@gmail.com>
2026-05-11 22:23:20 +0200

    frontend retry

--- client/src/components/common/Loader.jsx
+++
@@ -1,9 +0,0 @@
-const Loader = () => {
-  return (
-    <div className="flex items-center justify-center py-10">
-      <div className="h-10 w-10 animate-spin rounded-full border-4 border-slate-300 border-t-blue-600" />
-    </div>
-  );
-};
-
-export default Loader;
